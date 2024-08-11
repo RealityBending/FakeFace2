33d8ab5c480326b1a579eae11e95dc8a22cdfd5c
Dominique Makowski <dom.mak19@gmail.com>
2024-08-11 14:19:47 +0100

    add surveyswap message in consent form

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -47,31 +47,45 @@ var demographics_browser_info = {
 var demographics_consent = {
     type: jsPsychHtmlButtonResponse,
     css_classes: ["narrow-text"],
-    stimulus:
+    stimulus: function () {
         // Logo and title
-        "<img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>" +
-        "<h1>Informed Consent</h1>" +
-        // Overview
-        "<p align='left'><b>Invitation to Take Part</b><br>" +
-        "You are being invited to take part in a research study to further our understanding of human perception. Thank you for carefully reading this information sheet. This study is being conducted by Sofia Turcany Diaz and Dr Dominique Makowski from the School of Psychology, University of Sussex, who are happy to be contacted (st633@sussex.ac.uk and D.Makowski@sussex.ac.uk) if you have any questions.</p>" +
-        // Description
-        "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
-        "The goal is to study how new technology can impact <b>human perception</b>. In this study, you will be shown facial images and asked to complete a few questionnaires and perform some tasks. The whole experiment will take you <b>about 35 min</b> to complete. Please make you sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
-        // Results and personal information
-        "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
-        "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
-        "<p align='left'><b>Consent</b><br></p>" +
-        // Bullet points
-        "<li align='left'>I understand that by signing below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet</li>" +
-        "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
-        "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.</li>" +
-        "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
-        "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>" +
-        // Incentive
-        "<li align='left'>Please note that various checks will be performed to ensure the validity of the data. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>" +
-        "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate, simply close your browser.</li>" +
-        "</p>" +
-        "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Sofia Turcany Diaz (st633@sussex.ac.uk) or Dr Dominique Makowski (D.Makowski@sussex.ac.uk). This research has been approved (ER/ST633/1) by the ethics board of the School of Psychology. The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.</sup></sub></p>",
+        let text =
+            "<img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>" +
+            "<h1>Informed Consent</h1>"
+
+        if (jsPsych.data.urlVariables()["exp"] == "surveyswaptest") {
+            text +=
+                "<p style='color:green;' align='left'><b>Note: You will receive a <i style='color:purple;'>SurveySwap</i> completion code at the end of the experiment.</b></p>"
+        }
+
+        // Consent Form
+        text +=
+            // Overview
+            "<p align='left'><b>Invitation to Take Part</b><br>" +
+            "Thank you for considering to take part in this study that aims at deepening our understanding of human perception. " +
+            "This study is being conducted by Sofia Turcany Diaz and Dr Dominique Makowski from the <b>University of Sussex</b>, who are happy to be contacted if you have any questions (see contact information below).</p>" +
+            // Description
+            "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
+            "The goal is to study how new technology can impact <b>human perception</b>. In this study, you will be shown facial images and asked to complete a few questionnaires and perform some tasks. " +
+            "The whole experiment will take you <b style='color:green;'>~35 min</b> to complete. Please make you sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
+            // Results and personal information
+            "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
+            "The results of this research may be written into a scientific publication. Your anonymity will be ensured in the way described in the consent information below. <b>Please read this information carefully</b> and then, if you wish to take part, please acknowledge that you have fully understood this sheet, and that you consent to take part in the study as it is described here.</p>" +
+            "<p align='left'><b>Consent</b><br></p>" +
+            // Bullet points
+            "<li align='left'>I understand that by signing below I am agreeing to take part in the University of Sussex research described here, and that I have read and understood this information sheet</li>" +
+            "<li align='left'>I understand that my participation is entirely voluntary, that I can choose not to participate in part or all of the study, and that I can withdraw at any stage without having to give a reason and without being penalized in any way (e.g., if I am a student, my decision whether or not to take part will not affect my grades).</li>" +
+            "<li align='left'>I understand that since the study is anonymous, it will be impossible to withdraw my data once I have completed it.</li>" +
+            "<li align='left'>I understand that my personal data will be used for the purposes of this research study and will be handled in accordance with Data Protection legislation. I understand that the University's Privacy Notice provides further information on how the University uses personal data in its research.</li>" +
+            "<li align='left'>I understand that my collected data will be stored in a de-identified way. De-identified data may be made publicly available through secured scientific online data repositories.</li>" +
+            // Incentive
+            "<li align='left'>Please note that various checks will be performed to ensure the validity of the data. We reserve the right to withhold credit awards or reimbursement should we detect non-valid responses (e.g., random patterns of answers, instructions not read, ...).</li>" +
+            "<li align='left'>By participating, you agree to follow the instructions and provide honest answers. If you do not wish to participate, simply close your browser.</li>" +
+            "</p>" +
+            "<p align='left'><br><sub><sup>For further information about this research, or if you have any concerns, please contact Sofia Turcany Diaz (<i style='color:DodgerBlue;'>st633@sussex.ac.uk</i>) or Dr Dominique Makowski (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>). This research has been approved (ER/ST633/1) by the ethics board of the School of Psychology. The University of Sussex has insurance in place to cover its legal liabilities in respect of this study.</sup></sub></p>"
+
+        return text
+    },
     choices: ["I read, understood, and I consent"],
     data: { screen: "consent" },
 }
