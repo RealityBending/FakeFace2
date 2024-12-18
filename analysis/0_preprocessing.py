60d78fb5edacc16da03cbe23a4df336119ede22a
Dominique Makowski <dom.mak19@gmail.com>
2024-12-18 19:10:18 +0000

    update

--- analysis/0_preprocessing.py
+++ analysis/0_preprocessing.py
@@ -1,24 +1,13 @@
 import json
-
+import os
 import numpy as np
 import pandas as pd
 
 import requests
 
-# Load useful functions
-exec(
-    requests.get(
-        "https://raw.githubusercontent.com/RealityBending/scripts/main/data_OSF.py"
-    ).text
-)
+path = "C:/Users/domma/Box/Data/FakeFace2/"
+files = os.listdir(path)
 
-# Connect to OSF and get files --------------------------------------------
-token = ""  # Paste OSF token here to access private repositories
-files = osf_listfiles(  # Function in the data_OSF.py script loaded above
-    token=token,
-    data_subproject="s3kyn",  # Data subproject ID
-    after_date="21/07/2024",
-)
 
 # Loop through files ======================================================
 # Initialize empty dataframes
@@ -30,29 +19,34 @@ for i, file in enumerate(files):
     print(f"File N°{i+1}/{len(files)}")  # Print progress
 
     # Skip if participant already in the dataset
+    filename = file.replace(".csv", "")
     if (
         "Participant" in data_demo.columns
-        and file["name"] in data_demo["Participant"].values
+        and filename in data_demo["Participant"].values
     ):
         continue
 
-    data = osf_download(file)  # Function in the data_OSF.py script loaded above
+    data = pd.read_csv(path + file)
 
     # Participant ----------------------------------------------------------
-    data["screen"].unique()
+    # data["screen"].unique()
 
     # Browser info -------------------------------------------------------
+    if "browser_info" not in data["screen"].values:
+        continue
     browser = data[data["screen"] == "browser_info"].iloc[0]
 
     # Skip
-    if browser["researcher"] == "test":
+    if (
+        isinstance(browser["researcher"], str) is False
+        or browser["researcher"] == "test"
+    ):
         continue
 
     df = pd.DataFrame(
         {
-            "Participant": file["name"],
+            "Participant": filename,
             "Experiment_Duration": data["time_elapsed"].max() / 1000 / 60,
-            "Date_OSF": file["date"],
             "Date": browser["date"],
             "Time": browser["time"],
             "Browser": browser["browser"],
@@ -65,6 +59,14 @@ for i, file in enumerate(files):
         index=[0],
     )
 
+    df["Datetime"] = pd.to_datetime(
+        df["Date"] + " " + df["Time"], format="%d/%m/%Y %H:%M:%S"
+    )
+    if df["Datetime"].values[0] < pd.Timestamp("2024-07-20"):
+        continue
+    if "sona_id" in browser.index and not pd.isnull(browser["sona_id"]):
+        df["SONA_ID"] = int(browser["sona_id"])
+
     # Demographics -------------------------------------------------------
     demo = data[data["screen"] == "demographic_questions"].iloc[0]
     demo = json.loads(demo["response"])
@@ -76,7 +78,6 @@ for i, file in enumerate(files):
             demo[item] = "Other_" + answer
             item = item.replace("-Comment", "")
         df[item] = "Prefer not to say" if demo[item] == None else demo[item]
-
     # HEXACO ----------------------------------------------------------------
     hexaco = data[data["screen"] == "questionnaire_hexaco18"].iloc[0]
 
@@ -101,15 +102,16 @@ for i, file in enumerate(files):
     df["Feedback_SomeFacesAttractive"] = False
     df["Feedback_AIMoreAttractive"] = False
     df["Feedback_AILessAttractive"] = False
-    for f in f1["Feedback_1"]:
-        if "No face" in f:
-            df["Feedback_NoFacesAttractive"] = True
-        if "Some faces" in f:
-            df["Feedback_SomeFacesAttractive"] = True
-        if "more attractive" in f:
-            df["Feedback_AIMoreAttractive"] = True
-        if "less attractive" in f:
-            df["Feedback_AILessAttractive"] = True
+    if f1["Feedback_1"] is not None:
+        for f in f1["Feedback_1"]:
+            if "No face" in f:
+                df["Feedback_NoFacesAttractive"] = True
+            if "Some faces" in f:
+                df["Feedback_SomeFacesAttractive"] = True
+            if "more attractive" in f:
+                df["Feedback_AIMoreAttractive"] = True
+            if "less attractive" in f:
+                df["Feedback_AILessAttractive"] = True
 
     df["Feedback_DiffObvious"] = False
     df["Feedback_DiffSubtle"] = False
@@ -118,21 +120,22 @@ for i, file in enumerate(files):
     df["Feedback_LabelsReversed"] = False
     df["Feedback_AllReal"] = False
     df["Feedback_AllFake"] = False
-    for f in f1["Feedback_2"]:
-        if "obvious" in f:
-            df["Feedback_DiffObvious"] = True
-        if "subtle" in f:
-            df["Feedback_DiffSubtle"] = False
-        if "any difference" in f:
-            df["Feedback_DiffNone"] = True
-        if "not always correct" in f:
-            df["Feedback_LabelsIncorrect"] = True
-        if "reversed" in f:
-            df["Feedback_LabelsReversed"] = True
-        if "were photos" in f:
-            df["Feedback_AllReal"] = True
-        if "were AI-generated" in f:
-            df["Feedback_AllFake"] = True
+    if f1["Feedback_2"] is not None:
+        for f in f1["Feedback_2"]:
+            if "obvious" in f:
+                df["Feedback_DiffObvious"] = True
+            if "subtle" in f:
+                df["Feedback_DiffSubtle"] = False
+            if "any difference" in f:
+                df["Feedback_DiffNone"] = True
+            if "not always correct" in f:
+                df["Feedback_LabelsIncorrect"] = True
+            if "reversed" in f:
+                df["Feedback_LabelsReversed"] = True
+            if "were photos" in f:
+                df["Feedback_AllReal"] = True
+            if "were AI-generated" in f:
+                df["Feedback_AllFake"] = True
 
     df["Feedback_AllRealConfidence"] = (
         np.nan
@@ -165,7 +168,7 @@ for i, file in enumerate(files):
 
     dftask = pd.DataFrame(
         {
-            "Participant": file["name"],
+            "Participant": filename,
             "Stimulus": stims1["stimulus"],
             "Trial_Order_Phase1": stims1["trial_number"],
             "Trial_Duration_Phase1": stims1["trial_duration"] / 1000,
@@ -230,7 +233,7 @@ for i, file in enumerate(files):
             gaze = json.loads(row["webgazer_data"])
             dfgaze = pd.DataFrame(
                 {
-                    "Participant": file["name"],
+                    "Participant": filename,
                     "Stimulus": item.replace(".jpg", ""),
                     "Trial": row["trial_number"],
                     "Time": [g["t"] / 1000 for g in gaze],
@@ -263,7 +266,7 @@ for i, file in enumerate(files):
 
             dfgazefixcross = pd.DataFrame(
                 {
-                    "Participant": file["name"],
+                    "Participant": filename,
                     "Stimulus": item.replace(".jpg", ""),
                     "Trial": row["trial_number"],
                     "Time": [g["t"] / 1000 for g in fixcross],
@@ -284,26 +287,32 @@ for i, file in enumerate(files):
     data_demo = pd.concat([data_demo, df], axis=0, ignore_index=True)
 
 
+# SONA ====================================================================
+sona = data_demo.loc[data_demo["Source"] == "SONA"].sort_values("SONA_ID")
+sona[["SONA_ID"]].astype(int)
+data_demo = data_demo.drop(columns=["SONA_ID"])
+
 # Reanonimize =============================================================
-data_demo = data_demo.sort_values(["Date_OSF"])
+data_demo = data_demo.sort_values(["Datetime"])
 ppt = {s: f"S{i+1:03d}" for i, s in enumerate(data_demo["Participant"].unique())}
 data_demo["Participant"] = [ppt[s] for s in data_demo["Participant"]]
 data_task["Participant"] = [ppt[s] for s in data_task["Participant"]]
 data_eye["Participant"] = [ppt[s] for s in data_eye["Participant"]]
+data_demo = data_demo.drop(columns=["Datetime"])
 
 
-# Manual clean-up ---------------------------------------------------------
+# Manual clean-up =========================================================
 def replace_value(df, column, old, new):
     df = df.copy()
     df.loc[df[column] == old, column] = new
     return df
 
 
-# data_demo["Ethnicity"][data_demo["Ethnicity"].str.contains("Other_").values]
+# data_demo["Ethnicity"][data_demo["Ethnicity"].str.contains("Other_").values].values
 data_demo = replace_value(data_demo, "Ethnicity", "Other_White, Hispanic", "Mixed")
 
 
-# data_demo["Discipline"][data_demo["Discipline"].str.contains("Other_").values]
+# data_demo["Discipline"][data_demo["Discipline"].str.contains("Other_").values].values
 data_demo = replace_value(
     data_demo, "Discipline", "Other_Business Psychology", "Psychology"
 )
@@ -311,26 +320,55 @@ data_demo = replace_value(data_demo, "Discipline", "Other_Journalism", "Other")
 data_demo = replace_value(data_demo, "Discipline", "Other_Industrial Design", "Other")
 data_demo = replace_value(data_demo, "Discipline", "Other_pharmacy", "Other")
 data_demo = replace_value(data_demo, "Discipline", "Other_Geography", "Other")
+data_demo = replace_value(
+    data_demo, "Discipline", "Other_Health Sciences and Public Health", "Other"
+)
+data_demo = replace_value(data_demo, "Discipline", "Other_public health", "Other")
+data_demo = replace_value(
+    data_demo,
+    "Discipline",
+    "Other_Business: Human Resources and Industrial Relations\nEducation: Special Education",
+    "Other",
+)
+data_demo = replace_value(data_demo, "Discipline", "Other_Tourism", "Other")
 
-# data_demo["SexualOrientation"][data_demo["SexualOrientation"].str.contains("Other_").values]
+# data_demo["SexualOrientation"][data_demo["SexualOrientation"].str.contains("Other_").values].values
 data_demo = replace_value(data_demo, "SexualOrientation", "Other_Pansexual", "Other")
 data_demo = replace_value(
     data_demo, "SexualOrientation", "Other_Mainly heterosexual", "Heterosexual"
 )
+data_demo = replace_value(data_demo, "SexualOrientation", "Other_Queer", "Other")
+data_demo = replace_value(data_demo, "SexualOrientation", "Other_Demisexual", "Other")
+data_demo = replace_value(data_demo, "SexualOrientation", "Other_Questioning", "Other")
+data_demo = replace_value(data_demo, "SexualOrientation", "Other_Asexual", "Other")
+data_demo = replace_value(data_demo, "SexualOrientation", "Other_AroAce", "Other")
 
-# data_demo["SexualStatus"][data_demo["SexualStatus"].str.contains("Other_").values]
+
+# data_demo["SexualStatus"][data_demo["SexualStatus"].str.contains("Other_").values].values
 data_demo = replace_value(
     data_demo,
     "SexualStatus",
     "Other_Married not open to dating",
     "In a relationship and not open to dating",
 )
+data_demo = replace_value(
+    data_demo,
+    "SexualStatus",
+    "Other_Prefer not to say.",
+    np.nan,
+)
 
-
-# data_demo["Gender"][data_demo["Gender"].str.contains("Other_").values]
-# data_demo["Country"][data_demo["Country"].str.contains("Other_").values]
-# data_demo["Education"][data_demo["Education"].str.contains("Other_").values]
+# data_demo["Gender"][data_demo["Gender"].str.contains("Other_").values].values
+# data_demo["Country"][data_demo["Country"].str.contains("Other_").values].values
+# data_demo["Education"][data_demo["Education"].str.contains("Other_").values].values
 data_demo = replace_value(data_demo, "Education", "Other_Diploma in TEFL", "Other")
+data_demo = replace_value(data_demo, "Education", "Other_A level", "High school")
+data_demo = replace_value(data_demo, "Education", "Other_collage", "High school")
+data_demo = replace_value(
+    data_demo, "Education", "Other_A-Levels, college", "High school"
+)
+data_demo = replace_value(data_demo, "Education", "Other_Middle school", "Other")
+data_demo = replace_value(data_demo, "Education", "Other_Middle School", "Other")
 
 # Save data ==============================================================
 
